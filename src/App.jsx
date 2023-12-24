33f35f1f3f3a39db89c8411d7864d8c53cae1c5c
李名河 <2219845592@qq.com>
2023-12-24 16:24:45 +0800

    punctuation_grammar_translate

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
 import { PromptTemplate } from "langchain/prompts";
 import { StringOutputParser } from "langchain/schema/output_parser";
+import {
+  RunnableSequence,
+  RunnablePassthrough,
+} from "langchain/schema/runnable";
 
 import info from "./assets/scrimba-info.txt";
 import { useEffect } from "react";
@@ -69,15 +73,73 @@ answer:
     console.log(response);
   }
 
+  async function punctuation_grammar_translate(input) {
+    const punctuationTemplate = `Given a sentence, add punctuation where needed. 
+    sentence: {sentence}
+    sentence with punctuation:  
+    `;
+    const punctuationPrompt = PromptTemplate.fromTemplate(punctuationTemplate);
+
+    const grammarTemplate = `Given a sentence correct the grammar.
+    sentence: {punctuated_sentence}
+    sentence with correct grammar: 
+    `;
+    const grammarPrompt = PromptTemplate.fromTemplate(grammarTemplate);
+
+    const translationTemplate = `Given a sentence, translate that sentence into {language}
+    sentence: {grammatically_correct_sentence}
+    translated sentence:
+    `;
+    const translationPrompt = PromptTemplate.fromTemplate(translationTemplate);
+
+    const punctuationChain = RunnableSequence.from([
+      punctuationPrompt,
+      llm,
+      new StringOutputParser(),
+    ]);
+    const grammarChain = RunnableSequence.from([
+      grammarPrompt,
+      llm,
+      new StringOutputParser(),
+    ]);
+    const translationChain = RunnableSequence.from([
+      translationPrompt,
+      llm,
+      new StringOutputParser(),
+    ]);
+
+    const chain = RunnableSequence.from([
+      {
+        punctuated_sentence: punctuationChain,
+        original_input: new RunnablePassthrough(),
+      },
+      {
+        grammatically_correct_sentence: grammarChain,
+        // original_input: (original_input) => console.log(original_input),
+        language: ({ original_input }) => original_input.language,
+      },
+      translationChain,
+    ]);
+
+    const response = await chain.invoke({
+      sentence: input,
+      language: "chinese",
+    });
+
+    console.log(response);
+  }
+
   async function setup() {
+    const input = "i dont liked mondays";
+
     // 初始背景文档获取、分块、向量化、存储
     // await chunk_split_embedding_store();
 
-    // 根据input生成standAloneInput
-    await generate_standAloneInput_retrievel();
+    // 根据input生成standAloneInput，在向量数据库中查找最近解
+    // await generate_standAloneInput_retrievel();
 
-    // 在向量数据库中查找最近解
-    // 序列化处理原始input
+    // 序列化纠正原始input
+    // await punctuation_grammar_translate(input);
   }
 
   useEffect(() => setup, []);
