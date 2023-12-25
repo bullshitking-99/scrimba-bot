5c658984f683bf31afb1015467f363f3286aa7a2
李名河 <2219845592@qq.com>
2023-12-25 17:50:49 +0800

    wire up memory

--- src/App.jsx
+++ src/App.jsx
@@ -48,17 +48,19 @@ function App() {
   }
 
   async function standalone_retrievel_answer(input) {
-    const standaloneQuestionTemplate =
-      "Given a question, convert it to a standalone question. question: {question} standalone question:";
+    const standaloneQuestionTemplate = `Given some conversation history (if any) and a question, convert the question to a standalone question. 
+    conversation history: {conv_history}
+    question: {question} 
+    standalone question:`;
     const standaloneQuestionPrompt = PromptTemplate.fromTemplate(
       standaloneQuestionTemplate
     );
 
-    const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend. translate the answer to Chinese.
-context: {context}
-question: {question}
-answer: 
-`;
+    const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided and the conversation history. Try to find the answer in the context. If the answer is not given in the context, find the answer in the conversation history if possible. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+    context: {context}
+    conversation history: {conv_history}
+    question: {question}
+    answer: `;
     const answerPrompt = PromptTemplate.fromTemplate(answerTemplate);
 
     const standaloneQuestionChain = standaloneQuestionPrompt
@@ -81,12 +83,14 @@ answer:
       {
         context: retrieverChain,
         question: ({ original_input }) => original_input.question,
+        conv_history: ({ original_input }) => original_input.conv_history,
       },
       answerChain,
     ]);
 
     const response = await chain.invoke({
       question: input,
+      conv_history: convHistory.current,
     });
 
     return response;
